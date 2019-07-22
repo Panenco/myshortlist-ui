d02b4eb2095a48df7288746cc3e2ab2d31d3f54b
“Anton <chuprunant@gmail.com>
2019-07-22 14:12:46 +0300

    feat: orientation card

--- src/components/card/orientation/index.jsx
+++ src/components/card/orientation/index.jsx
@@ -1,40 +1,72 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Card } from 'components/card/base';
-import { PrimaryButton } from 'components/button';
+import { PrimaryButton, SecondaryButton } from 'components/button';
 import cx from 'classnames';
 import s from './styles.scss';
 
-const OrientationCard = ({ children, info, bought, free, className }) => (
-  <Card className={cx(s.card, className)}>
-    <div className={s.cardHeader}>
-      <div className={s.cardHeaderTitle}>Your personal brand</div>
-      <div className={cx(s.cardHeaderDate, bought && s.cardHeaderDateBought)}>{bought && 'Bought '}09/07/2018</div>
-    </div>
-
-    <div className={s.cardBody}>{children}</div>
-
-    {free ? (
-      <div className={s.cardFooter}>
-        <span className={s.cardFooterFree}>free</span>
-        <PrimaryButton className={s.cardFooterPurchase}>purchase for free</PrimaryButton>
+const OrientationCard = ({ children, info, free, price, bought, done, className }) => {
+  const doneRate = {
+    width: `${(done * 100) / 7}%`,
+  };
+  return (
+    <Card className={cx(s.card, className)}>
+      <div className={s.cardHeader}>
+        <div className={s.cardHeaderTitle}>Your personal brand</div>
+        <div className={cx(s.cardHeaderDate, bought && s.cardHeaderDateBought)}>{bought && 'Bought '}09/07/2018</div>
       </div>
-    ) : (
-      <div className={s.cardFooter}>
-        <span className={s.cardFooterPrice}>14,99Є</span>
-        <PrimaryButton className={s.cardFooterPurchase}>purchase for 14,99Є</PrimaryButton>
-      </div>
-    )}
-  </Card>
-);
+
+      <div className={s.cardBody}>{children}</div>
+
+      <Link to="/" className={s.cardMore}>
+        Show more
+      </Link>
+
+      {free && (
+        <div className={s.cardFooter}>
+          <span className={s.cardFooterFree}>free</span>
+          <PrimaryButton className={s.cardFooterPurchase}>purchase for free</PrimaryButton>
+        </div>
+      )}
+
+      {price && (
+        <div className={s.cardFooter}>
+          <span className={s.cardFooterPrice}>14,99Є</span>
+          <PrimaryButton className={s.cardFooterPurchase}>purchase for 14,99Є</PrimaryButton>
+        </div>
+      )}
+
+      {bought && (
+        <div className={s.cardFooter}>
+          <div className={s.cardFooterRate}>
+            <span className={s.cardFooterRateDone}>{done} out of 7 sections done</span>
+            <div className={s.cardFooterRateIndicator}>
+              <div className={s.cardFooterRateIndicatorDoneLine} style={doneRate} />
+            </div>
+          </div>
+          <SecondaryButton className={s.cardFooterPurchase}>continue testing</SecondaryButton>
+        </div>
+      )}
+    </Card>
+  );
+};
 
 OrientationCard.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  price: PropTypes.number,
+  done: PropTypes.number,
+  bought: PropTypes.bool,
+  free: PropTypes.bool,
 };
 
 OrientationCard.defaultProps = {
   className: null,
+  price: null,
+  done: null,
+  bought: false,
+  free: false,
 };
 
 export { OrientationCard };
--- stories/card/index.jsx
+++ stories/card/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { OrientationCard, Row, Col } from 'index';
 import { decorator } from '../../utils/decorator';
 
 export default decorator('Card').add('OrientationCard Component', () => (
-  <>
+  <BrowserRouter>
     <Row>
       <Col s="12">
         <OrientationCard free>
@@ -15,11 +16,20 @@ export default decorator('Card').add('OrientationCard Component', () => (
 
     <Row>
       <Col s="12">
-        <OrientationCard>
+        <OrientationCard price>
           Here you will perform a couple of test with your network to give you feedback from a 360 degree angle. Here
           you will qwerty qwerty qwerty qwerty qwerty qwerty
         </OrientationCard>
       </Col>
     </Row>
-  </>
+
+    <Row>
+      <Col s="12">
+        <OrientationCard bought done={4}>
+          Here you will perform a couple of test with your network to give you feedback from a 360 degree angle. Here
+          you will qwerty qwerty qwerty qwerty qwerty qwerty
+        </OrientationCard>
+      </Col>
+    </Row>
+  </BrowserRouter>
 ));
