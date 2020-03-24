a45173fa37227f45eb36ff00c37396236627e436
Vladyslav Symonenko <vladyslavsymonenko@Vladyslavs-MacBook-Pro.local>
2020-03-24 10:14:28 +0200

    fix: table styles

--- stories/avatar/index.jsx
+++ stories/avatar/index.jsx
@@ -5,8 +5,14 @@ import { decorator } from '../../utils/decorator';
 export default decorator('Avatar').add('Avatar Component', () => (
   <>
     <Row>
-      <Col s="12">
+      <Col s="5">
         <Avatar header />
+      </Col>
+      <Col s="5">
+        <Avatar
+          header
+          imgSrc="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        />
       </Col>
     </Row>
   </>
