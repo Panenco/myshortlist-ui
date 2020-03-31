08f3206f9bef155f89f43a96ec7cc73c98f4f6ca
Vladyslav Symonenko <vladyslavsymonenko@Vladyslavs-MacBook-Pro.local>
2020-03-31 14:11:22 +0300

    feat: static server with config for heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+// const request = require('request');
+
+const express = require('express');
+
+const compression = require('compression');
+const uiPaths = require('./config/paths');
+
+const port = process.env.PORT || 80;
+const app = express();
+
+app.use(compression());
+
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', '*');
+//     res.header('Access-Control-Allow-Headers', '*');
+//     next();
+//   });
+// }
+
+app.use('/', express.static(uiPaths.storybook));
+
+app.listen(port, () => console.log(`App is now running on ${port}`));
