269716a7b6e5a6932047cf7f492857269e4b8df8
Vladyslav Symonenko <vladyslavsymonenko@Vladyslavs-MacBook-Pro.local>
2020-04-21 16:40:37 +0300

    fix: refactor upload image component

--- src/components/input/file-image/index.jsx
+++ src/components/input/file-image/index.jsx
@@ -21,23 +21,27 @@ class ImageDrop extends React.Component {
 
   renderDropzoneContent = fileProps => {
     const { value } = this.props;
-
-    if (value && value.length) {
-      return (
-        <>
-          <img className={s.imgDropAvatar} src={URL.createObjectURL(value[0])} alt="avatar" />
-          <input {...fileProps.getInputProps()} />
-        </>
-      );
-    }
     return (
       <>
-        <img
-          className={s.imgDropAvatar}
-          src="http://www.colonialkc.org/wp-content/uploads/2015/07/placeholder_square.jpg"
-          alt="avatar"
-        />
-        <input {...fileProps.getInputProps()} />
+        {value ? (
+          <>
+            <img
+              className={s.imgDropAvatar}
+              src={value && value[0] instanceof File ? URL.createObjectURL(value[0]) : value}
+              alt="avatar"
+            />
+            <input {...fileProps.getInputProps()} />
+          </>
+        ) : (
+          <>
+            <img
+              className={s.imgDropAvatar}
+              src="http://www.colonialkc.org/wp-content/uploads/2015/07/placeholder_square.jpg"
+              alt="avatar"
+            />
+            <input {...fileProps.getInputProps()} />
+          </>
+        )}
       </>
     );
   };
