cfe786d58376a317d4a02812b4413b3d813b1136
Vladyslav Symonenko <vladyslavsymonenko@Vlad.local>
2020-09-16 11:59:52 +0300

    feat: add date-picker to ui

---
+++ src/components/datepicker-material/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { makeStyles } from '@material-ui/core/styles';
+import cx from 'classnames';
+import styles from './styles.scss';
+
+const useDatePickerStyles = makeStyles(() => ({
+  root: {
+    '& .Mui-focused': {
+      // outline: 'none !important',
+      outline: '2px solid #26a69a',
+    },
+    '& .MuiFormHelperText-root': {
+      display: 'none',
+    },
+    '& .MuiSvgIcon-root': {
+      width: '20px',
+      height: '20px',
+    },
+  },
+}));
+
+const DatePicker = props => {
+  const s = useDatePickerStyles();
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        disableToolbar
+        variant="outlined"
+        format="MM/dd/yyyy"
+        // margin="normal"
+        id="date-picker-inline"
+        // label="Date picker inline"
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}
+        {...props}
+        className={cx(styles.root, s.root)}
+      />
+    </MuiPickersUtilsProvider>
+  );
+};
+
+export default DatePicker;
