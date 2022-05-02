0a76fddfd46939e4b675e85edbd35d4909fc7d69
sebastiaanviaene <sebviaene@gmail.com>
2022-05-02 13:16:59 +0200

    fix: tryfix webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const { configureActions } = require('@storybook/addon-actions');
+const webpack = require('webpack');
+const paths = require('../config/paths');
+
 module.exports = {
   core: {
     builder: 'webpack5',
@@ -9,4 +13,61 @@ module.exports = {
     '@storybook/addon-options',
     'storybook-readme/register',
   ],
+  framework: '@storybook/react',
+  stories: ['../stories/index.jsx'],
+
+  webpackFinal: async (config, { configType }) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      extensions: ['.js', '.jsx'],
+      modules: ['node_modules', paths.src],
+      alias: {
+        config: paths.appConfig,
+        static: paths.publicFiles,
+        public: paths.publicFiles,
+      },
+    },
+    module: {
+      ...config.module,
+
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.svg$/,
+          exclude: [paths.publicFiles],
+          loader: 'svg-sprite-loader',
+        },
+        {
+          test: /\.css$/,
+          sideEffects: true,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          sideEffects: true,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[local]-[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [paths.scss],
+              },
+            },
+          ],
+        },
+        {
+          include: [paths.publicFiles],
+          loader: 'file-loader',
+        },
+      ],
+    },
+  }),
 };
